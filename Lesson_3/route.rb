3bc4b9c62e3091fbac848f869b8654a5126b427d
iboyards <iboyards@gmail.com>
2016-05-13 13:56:28 +0300

    Replace routed => station, add <<

--- Lesson_3/route.rb
+++ Lesson_3/route.rb
@@ -1,32 +1,22 @@
-class Route
-	
-
-	attr_accessor :routed, :i
+class Route	
+	attr_accessor :stations, :i
 	attr_reader :first_st, :last_st
 
 	def initialize(first_st, last_st)
-		@routed = []	
+		@stations = []	
 		@i = 1	
-		@routed[0] = first_st 
-		@last_st = last_st
-		@routed[1] = last_st
-    end
+		@stations << first_st << last_st
+	end
   
 	def add(station)
-		@routed[@i] = station
-		@i = @routed.size 
-		@routed[@i] = @last_st
+		@stations.insert(@i,station)
+		@i = @stations.size 
 	end
 
 	def del(station)
-		@routed.delete(station)
-		print @routed
+		@stations.delete(station)
+		print @stations
 	end
-
-	def list
-		puts @routed
-	end
-
 end
 
 
